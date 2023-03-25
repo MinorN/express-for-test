1fd1efe0e7b7c46fb26781377a31c9b0849c40c1
qijiajun <1481700559@qq.com>
2023-03-25 10:37:43 +0800

    use ts

--- app2.ts
+++ app2.ts
@@ -1,11 +1,45 @@
-import express, {request, response} from 'express';
+import {ErrorRequestHandler, RequestHandler} from 'express';
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+
 const app = express();
-const port = 3000;
 
-app.get('/', (request, response) => {
-    response.send('你好啊，我是app2.ts');
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) {
+    next(createError(404));
+} as RequestHandler);
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+    next()
+} as ErrorRequestHandler);
+
+module.exports = app;
